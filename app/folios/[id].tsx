30d9a01007e065a33e038fb1eadcd8636d361921
Jesus Yañez <jesusyanezx01@gmail.com>
2025-02-06 17:55:26 -0500

    Implementation of detailed view of folios

--- app/folios/[id].tsx
+++ app/folios/[id].tsx
@@ -1,14 +1,15 @@
-import { View, Text, ScrollView, StyleSheet } from "react-native";
+import { View, Text, ScrollView, StyleSheet, Pressable } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import CustomBadge from "@/components/CustomBadge";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { LinearGradient } from "expo-linear-gradient";
+import OptionsList from "@/components/OptionsList";
 
 export default function FolioDetailScreen() {
   const { id } = useLocalSearchParams();
 
   return (
-    <ScrollView>
+    <ScrollView style={{ backgroundColor: "#fff" }}>
       <LinearGradient
         colors={["#5E0FDD", "#A036E4"]}
         start={{ x: 0, y: 0 }}
@@ -34,7 +35,6 @@ export default function FolioDetailScreen() {
             style={{
               fontSize: 24,
               marginBottom: 8,
-              marginStart: 16,
               fontWeight: "bold",
               color: "white",
             }}
@@ -46,7 +46,7 @@ export default function FolioDetailScreen() {
           >
             <CustomBadge
               label="correctiva"
-              styleView={{ backgroundColor: "#4a01c4", marginStart: 16 }}
+              styleView={{ backgroundColor: "#4a01c4" }}
             />
             <View style={styles.showMoreContainer}>
               <Text style={{ color: "white" }}>Ver más</Text>
@@ -60,10 +60,22 @@ export default function FolioDetailScreen() {
         </View>
       </LinearGradient>
 
-      <Text>15 Dias transcurridos</Text>
+      <View style={{ backgroundColor: "#ec6666" }}>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 20,
+            paddingHorizontal: 24,
+            paddingVertical: 8,
+            fontWeight: "bold",
+          }}
+        >
+          15 Dias transcurridos
+        </Text>
+      </View>
 
-      <View>
-        <Text>Descripcion</Text>
+      <View style={styles.descriptionContainer}>
+        <Text style={{ fontWeight: "bold", marginBottom: 8 }}>Descripcion</Text>
         <Text>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, itaque
           ullam dolorum corporis at ab harum explicabo corrupti cumque quas esse
@@ -71,20 +83,41 @@ export default function FolioDetailScreen() {
         </Text>
       </View>
 
-      <View>
-        <Text>Evidencias</Text>
-        <Text>Opciones avanzadas</Text>
-        <Text>Informe de folio</Text>
-        <Text>Comentarios</Text>
+      <View style={styles.optionsListContainer}>
+        <OptionsList />
       </View>
 
-      <View>
-        <Text>Historial</Text>
-        <Text>Pedir cotizacion</Text>
-        <Text>Proveedor:</Text>
+      <View style={styles.historyContainer}>
+        <Text
+          style={{
+            fontWeight: "bold",
+            marginBottom: 8,
+            borderBottomColor: "#e4e4e4",
+            borderBottomWidth: 1,
+          }}
+        >
+          Historial
+        </Text>
 
-        <Text>Reasignar proveedor</Text>
-        <Text>Pedir cotizacion</Text>
+        <View style={styles.card}>
+          <Text style={{ color: "#3a87d9", fontSize: 20, fontWeight: "bold" }}>
+            Pedir cotizacion
+          </Text>
+          <Text style={{ color: "#b7b7b7" }}>Proveedor: Jesus Yañez</Text>
+
+          <Pressable
+            onPress={() => console.log("hola")}
+            style={styles.buttonPrimary}
+          >
+            <Text style={styles.buttonTextPrimary}>Reasignar proveedor</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => console.log("hola")}
+            style={styles.buttonSecondary}
+          >
+            <Text style={styles.buttonTextSecondary}>Pedir cotizacion</Text>
+          </Pressable>
+        </View>
       </View>
     </ScrollView>
   );
@@ -92,16 +125,56 @@ export default function FolioDetailScreen() {
 
 const styles = StyleSheet.create({
   mainContentContainer: {
-    margin: 16,
+    marginHorizontal: 24,
+    marginVertical: 16,
+  },
+  descriptionContainer: {
+    marginHorizontal: 24,
+    marginTop: 16,
+  },
+  optionsListContainer: {
+    marginHorizontal: 24,
+    marginTop: 16,
+  },
+  historyContainer: {
+    marginHorizontal: 24,
+    marginTop: 8,
   },
   badgeContainer: {
     flexDirection: "row",
     gap: 4,
     marginBottom: 8,
     marginTop: 8,
-    marginStart: 16,
   },
   showMoreContainer: {
     flexDirection: "row",
+    gap: 8,
   },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    padding: 20,
+    marginTop: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  buttonPrimary: {
+    marginTop: 8,
+    alignItems: "center",
+    borderColor: "#3a87d9",
+    borderWidth: 1,
+    padding: 16,
+    borderRadius: 4,
+  },
+  buttonSecondary: {
+    backgroundColor: "#3a87d9",
+    marginTop: 8,
+    alignItems: "center",
+    padding: 16,
+    borderRadius: 4,
+  },
+  buttonTextPrimary: { color: "#3a87d9", fontSize: 16  },
+  buttonTextSecondary: { color: "#fff", fontSize: 16 },
 });
---
+++ components/OptionsList.tsx
@@ -0,0 +1,100 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import React from "react";
+
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+type Props = {
+  title: string;
+  iconName: string;
+  colorIcon: string;
+  showBadge?: boolean;
+  onPress?: () => void;
+};
+
+const OptionItem = ({
+  title,
+  iconName,
+  colorIcon,
+  showBadge,
+  onPress,
+}: Props) => {
+  return (
+    <TouchableOpacity style={styles.optionContainer} onPress={onPress}>
+      <View style={{ flexDirection: "row", gap: 8 }}>
+        <Text style={styles.optionText}>{title}</Text>
+        {showBadge && (
+          <View style={styles.badge}>
+            <Text style={styles.badgeText}>1</Text>
+          </View>
+        )}
+      </View>
+      <View style={styles.iconContainer}>
+        <Ionicons name={iconName} size={24} color={colorIcon} />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const OptionsList = () => {
+  return (
+    <View>
+      <OptionItem title="Evidencias" iconName="attach" colorIcon="#5511c8" />
+      <OptionItem
+        title="Opciones avanzadas"
+        iconName="arrow-forward"
+        colorIcon="black"
+      />
+      <OptionItem
+        title="Informe de folio"
+        iconName="eye-outline"
+        colorIcon="black"
+      />
+      <OptionItem
+        title="Comentarios"
+        iconName="arrow-forward"
+        showBadge
+        colorIcon="black"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  optionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  optionText: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  iconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  badge: {
+    backgroundColor: "red",
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginRight: 5,
+  },
+  badgeText: {
+    color: "#FFF",
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+});
+
+export default OptionsList;
